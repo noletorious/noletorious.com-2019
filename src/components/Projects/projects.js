b611a0d0df8db9937d552e0af740e07856a71443
TRI-MET\torresn <torresn@trimet.org>
2019-08-16 16:18:10 -0700

    update projects homepage section

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -112,72 +112,72 @@ const Projects = () => {
     return(
         <Container className="vh-100" id='work' fluid>
             <Row className="h-50">
-                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pl-0'].join(' ')} style={trimetBg}>
+                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pb-3'].join(' ')} >
                   <SpringLink to="/trimet" >
-                    <div className="d-flex flex-column h-100">
-                      <div className={['p-2','d-flex','flex-row', projectStyles.projectTitleLogo].join(' ')}>
+                    <div className={['h-100','d-flex','flex-column', projectStyles.projectTitleLogo].join(' ')} style={trimetBg}>
+                      <div className={['p-2','d-flex','flex-row','mb-auto', projectStyles.projectTitleLogo].join(' ')}>
                         <div className="align-self-center">
-                          <span className="h4">TriMet</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
+                          <span className="h5">TriMet</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
                         </div>
                         <div className="ml-auto align-self-center">
-                          <Img fluid={data.trimetLogo.childImageSharp.fluid} style={{width:'50px'}}/>
+                          <Img fluid={data.trimetLogo.childImageSharp.fluid} style={{width:'35px'}}/>
                         </div>
                       </div>
                       <div className="d-flex">
-                        <p className={['pb-2','pt-1','px-2', projectStyles.perProjectText].join(' ')}>UX Design, Visual Design, Motion Graphics, Video/Content Production, Animation Storytelling, React Front-end Development, Photography</p>
+                        <p className={['pt-1','px-2','small', projectStyles.perProjectText].join(' ')}>UX Design, Visual Design, Motion Graphics, Video/Content Production, Animation Storytelling, React Front-end Development, Photography</p>
                       </div>
                     </div>
                   </SpringLink>
                 </Col>
-                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pl-0'].join(' ')} style={attensaBg}>
+                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pb-3'].join(' ')}>
                   <SpringLink to="/attensa" >
-                    <div className="d-flex flex-column h-100">
-                      <div className={['p-2','d-flex','flex-row', projectStyles.projectTitleLogo].join(' ')}>
+                    <div className="d-flex flex-column h-100" style={attensaBg}>
+                      <div className={['p-2','d-flex','flex-row','mb-auto', projectStyles.projectTitleLogo].join(' ')}>
                         <div className="align-self-center">
-                          <span className="h4">Attensa</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
+                          <span className="h5">Attensa</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
                         </div>
                         <div className="ml-auto align-self-center">
-                          <Img fluid={data.attensaLogo.childImageSharp.fluid} style={{width:'50px'}}/>
+                          <Img fluid={data.attensaLogo.childImageSharp.fluid} style={{width:'35px'}}/>
                         </div>
                       </div>
                       <div className="d-flex">
-                        <p className={['pb-2','pt-1','px-2', projectStyles.perProjectText].join(' ')}>UX Design, Visual Design, Content/Strategic Marketing</p>
+                        <p className={['pt-1','px-2','small', projectStyles.perProjectText].join(' ')}>UX Design, Visual Design, Content/Strategic Marketing</p>
                       </div>
                     </div>
                   </SpringLink>
                 </Col>
             </Row>
             <Row className="h-50">
-                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pl-0'].join(' ')} style={ctBg}>
+                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pb-3'].join(' ')}>
                   <SpringLink to="/attensa" >
-                    <div className="d-flex flex-column h-100">
-                      <div className={['p-2','d-flex','flex-row', projectStyles.projectTitleLogo].join(' ')}>
+                    <div className="d-flex flex-column h-100" style={ctBg}>
+                      <div className={['p-2','d-flex','flex-row','mb-auto', projectStyles.projectTitleLogo].join(' ')}>
                         <div className="align-self-center">
-                          <span className="h4">Cascadia Threads</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
+                          <span className="h5">Cascadia Threads</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
                         </div>
                         <div className="ml-auto align-self-center">
-                          <Img fluid={data.ctLogo.childImageSharp.fluid} style={{width:'50px'}}/>
+                          <Img fluid={data.ctLogo.childImageSharp.fluid} style={{width:'45px'}}/>
                         </div>
                       </div>
                       <div className="d-flex">
-                        <p className={['pb-2','pt-1','px-2', projectStyles.perProjectText].join(' ')}>Design Lead</p>
+                        <p className={['pt-1','px-2','small', projectStyles.perProjectText].join(' ')}>Design Lead</p>
                       </div>
                     </div>
                   </SpringLink>
                 </Col>
-                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pl-0'].join(' ')} style={hopBg}>
+                <Col xs={{span:12}} sm={{span:6}} className={['text-white','pb-3'].join(' ')}>
                   <SpringLink to="/hop" >
-                    <div className="d-flex flex-column h-100">
-                      <div className={['p-2','d-flex','flex-row', projectStyles.projectTitleLogo].join(' ')}>
+                    <div className="d-flex flex-column h-100"  style={hopBg}>
+                      <div className={['p-2','d-flex','flex-row','mb-auto', projectStyles.projectTitleLogo].join(' ')}>
                         <div className="align-self-center">
-                          <span className="h4">Hop Fastpass</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
+                          <span className="h5">Hop Fastpass</span> <FontAwesomeIcon icon={faLongArrowAltRight}></FontAwesomeIcon>
                         </div>
                         <div className="ml-auto align-self-center">
-                          <Img fluid={data.hopLogo.childImageSharp.fluid} style={{width:'50px'}}/>
+                          <Img fluid={data.hopLogo.childImageSharp.fluid} style={{width:'35px'}}/>
                         </div>
                       </div>
                       <div className="d-flex">
-                        <p className={['pb-2','pt-1','px-2', projectStyles.perProjectText].join(' ')}>Hand modeling, video production, motion graphics, 2D animation, UI Design</p>
+                        <p className={['pt-1','px-2','small', projectStyles.perProjectText].join(' ')}>Hand modeling, video production, motion graphics, 2D animation, UI Design</p>
                       </div>
                     </div>
                   </SpringLink>
