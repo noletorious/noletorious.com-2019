19d306e61891247f309eeccad4fee96cade1addb
TRI-MET\torresn <torresn@trimet.org>
2019-08-18 01:28:37 -0700

    attensa page

--- src/pages/attensa.js
+++ src/pages/attensa.js
@@ -6,7 +6,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../layouts/page"
 import SEO from "../components/seo"
 import Img from 'gatsby-image'
-
+import attensaLogoAnimation from '../images/attensa-animation.gif'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons'
 
@@ -76,6 +76,51 @@ const Attensa = () => {
       }
     }
   }
+  mobileLoginImage: file(relativePath: { eq: "attensa-mobLogin.png" }) {
+    childImageSharp {
+      fluid(
+        quality: 100
+      ) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  mobileMyTopicsImage: file(relativePath: { eq: "attensa-mobMyTopics.png" }) {
+    childImageSharp {
+      fluid(
+        quality: 100
+      ) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  mobileReadViewImage: file(relativePath: { eq: "attensa-mobReadView.png" }) {
+    childImageSharp {
+      fluid(
+        quality: 100
+      ) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  mobileTopicViewImage: file(relativePath: { eq: "attensa-mobTopicView.png" }) {
+    childImageSharp {
+      fluid(
+        quality: 100
+      ) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  landingPageImage: file(relativePath: { eq: "attensa-LP.png" }) {
+    childImageSharp {
+      fluid(
+        quality: 100
+      ) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
 }
 `)
 
@@ -100,7 +145,6 @@ return (
         <Row>
           <Col sm={{span:12}} md={{span:12}}>
             <Img fluid={data.shoulderImage.childImageSharp.fluid} fadeIn={true}/>
-            <p className='small text-muted text-right pt-1'>Morning standup</p>
           </Col>
         </Row>
         <Row>
@@ -112,7 +156,7 @@ return (
           &nbsp;
           <h3>Problem:</h3>
           &nbsp;
-          <div className="rounded bg-secondary mb-4">
+          <div className="rounded mb-4">
             <Img fluid={data.silosImage.childImageSharp.fluid} fadeIn={true}/>
           </div>
           <p>Disseminating information within a company is a lot easier said than done. Especially if your company is a large enterprise with different departments with siloed communication channels.</p>
@@ -120,19 +164,67 @@ return (
           <h3>Attensa Solution:</h3>
           &nbsp;
           <p>Give topic experts a platform to manage information sources and give them the power control over how their content is recieved by their audience.</p>
-          <Img fluid={data.solOneImage.childImageSharp.fluid} fadeIn={true}/>
-          <Img fluid={data.solTwoImage.childImageSharp.fluid} fadeIn={true}/>
+          <Img fluid={data.solOneImage.childImageSharp.fluid} className="my-5" fadeIn={true}/>
+          <Img fluid={data.solTwoImage.childImageSharp.fluid} className="my-5" fadeIn={true}/>
           &nbsp;
           <h3 className="mt-5">Strategy/Design:</h3>
-          <p>During my time at Attensa, I helped design the Topic view segment of the app. A Topic is essentially a container managed by content curators, that consumers can subscribe or be assigned to depending on how the organization setup.</p>
+          <p>During my time at Attensa, I helped design the Topic view segment of the app. A Topic is essentially a container managed by content curators, that consumers can subscribe or be assigned to depending on how the organization setup. I collaborated closely with product managers on working through and exploring UX flows on main tasks as well as micro-interactions.</p>
           &nbsp;
+          <p>This does not reflect the current product, but here are a few screen mockup I can share:</p>
           </Col>
         </Row>
         <Row>
           <Col sm={{span:12}} md={{span:12}}>
             <div className="w-100 py-4">
               <Img className='mx-auto' fluid={data.topicViewImage.childImageSharp.fluid} style={{maxWidth:'900px'}} fadeIn={true}/>
+              <p className="small text-muted text-center">Topic View on the consumer side</p>
             </div>            
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={{span:2}} sm={{span:3}}>
+            <div className="w-100 py-4">
+              <Img className='mx-auto' fluid={data.mobileLoginImage.childImageSharp.fluid} style={{maxWidth:'900px'}} fadeIn={true}/>
+              <p className="small text-muted text-center">App Login Screen</p>
+            </div>            
+          </Col>
+          <Col xs={{span:2}} sm={{span:3}}>
+            <div className="w-100 py-4">
+              <Img className='mx-auto' fluid={data.mobileMyTopicsImage.childImageSharp.fluid} style={{maxWidth:'900px'}} fadeIn={true}/>
+              <p className="small text-muted text-center">My Topics View displaying relevancy</p>
+            </div>            
+          </Col>
+          <Col xs={{span:2}} sm={{span:3}}>
+            <div className="w-100 py-4">
+              <Img className='mx-auto' fluid={data.mobileReadViewImage.childImageSharp.fluid} style={{maxWidth:'900px'}} fadeIn={true}/>
+              <p className="small text-muted text-center">Reading View Example</p>
+            </div>            
+          </Col>
+          <Col xs={{span:2}} sm={{span:3}}>
+            <div className="w-100 py-4">
+              <Img className='mx-auto' fluid={data.mobileTopicViewImage.childImageSharp.fluid} style={{maxWidth:'900px'}} fadeIn={true}/>
+              <p className="small text-muted text-center">Topic View of NYT Example</p>
+            </div>            
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+          &nbsp;
+          <h3 className="mt-5">Visual Design:</h3>
+          <p>As I used the product myself, I started to really understand the luxuries of having content curated for me with AI and delivered to me when I specify. This taught me to design with empathy toward what users want. Not only product design but also for pages for potential customers.</p>
+          &nbsp;
+          <Img fluid={data.landingPageImage.childImageSharp.fluid} className="my-5" fadeIn={true}/>
+          &nbsp;
+          <p>I spent most of my time designing responsive email templates for each customer instance, infographics as long-tail content for marketing purposes, and mockup landing pages for team buy off as well as the landing page itself.</p>
+          &nbsp;
+          </Col>
+        </Row>
+    </Container>
+    <Container fluid style={{background:"#323232",paddingTop:'100px',paddingBottom:'100px'}}>
+      <Row>
+          <Col sm={{span:12}} md={{span:12}}>
+            &nbsp;
+            <img src={attensaLogoAnimation} className="mx-auto d-block" style={{maxWidth:'600px'}} alt="Attensa Motion" />
           </Col>
         </Row>
     </Container>
--- src/pages/trimet.js
+++ src/pages/trimet.js
@@ -41,6 +41,42 @@ const projects = [
     name: 'Open Trip Planner',
     status: 'WIP',
     anchorLink: 'otp'
+  },
+  {
+    year:'2019',
+    name: 'New Onsite Transit Tracker',
+    status: 'WIP',
+    anchorLink: 'onsite'
+  },
+  {
+    year:'2019',
+    name: 'Google Earth Flythroughs',
+    status: 'Ongoing',
+    anchorLink: 'flythroughs'
+  },
+  {
+    year:'2019',
+    name: 'Photography: Instagram',
+    status: 'Ongoing',
+    anchorLink: 'insta'
+  },
+  {
+    year:'2018',
+    name: 'PDX Arrival Screens',
+    status: 'Phase II',
+    anchorLink: 'pdxArrivals'
+  },
+  {
+    year:'2018',
+    name: 'New Club, New Blog',
+    status: 'Complete',
+    anchorLink: 'ridersClub'
+  },
+  {
+    year:'2017',
+    name: 'Web Style Guide Lines',
+    status: 'Ongoing',
+    anchorLink: 'stylesGuide'
   }
 ]
 return (
@@ -66,7 +102,7 @@ return (
             <Img fluid={data.tilikumImage.childImageSharp.fluid} fadeIn={true}/>
           </Col>
         </Row>
-        <Row>
+        <Row className='my-5'>
           <Col sm={{span:12}} md={{span:8, offset:2}}>
           &nbsp;
           <h3>RiderComm</h3>
@@ -100,12 +136,89 @@ return (
               }
             </tbody>
           </Table>
-          &nbsp;
-          <div className="py-3"></div>
-          &nbsp;
+          &nbsp;          
+          </Col>
+        </Row>
+        {/* Open Trip Planner */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
           <h3 id="otp">Open Trip Planner</h3>
           <p>This project is still on going so I’m limited to what I can share. This project deserves its own page.</p>
           <a href="http://modbeta.trimet.org/">Enjoy beta <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
+          </Col>
+        </Row>
+        {/* Onsite Transit Tracker */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+          <h3 id="onsite">New Onsite Transit Tracker</h3>
+          <p>Improving the UI to arrival information for riders is just straight up fun. It is great to be involved in a design that has such high visibility within the town I live in.</p>
+          &nbsp;
+          <h5>Problem:</h5>
+          <p>With the consideration of upgrading our display screens out in the public, we needed a refresh to the new design. With new infrastructure came new capabilities.</p>
+          <p>[insert image of new tracker</p>
+          <h5>Research:</h5>
+          <p>Questions we asked our riders:</p>
+          <p><em>Is the current transit tracker useful?</em></p>
+          <p><strong>Purpose of asking:</strong> So we can get a sense of any immediate red flags that people dislike about it. </p>
+          <p><strong>Results:</strong> Yes, turns out people trust the information as being more reliable than third-party apps.</p>
+          <p><em>What other information would they like to know at their stop?</em></p>
+          <p><strong>Purpose of asking:</strong> So we can get a sense of what people want to generally know when looking at these screens.</p>
+          <p><strong>Results:</strong> The top want is, vehicle location and it’s relation to where they are.</p>
+          <h5>Strategy:</h5>
+          <p>With the knowledge we gained by simply asking riders, jump started the design. The two big improvements I wanted to solve was:</p>
+          <ul>
+            <li>Visibility, increasing contrast and improving the ADA compliance score.</li>
+            <li>Context, at a glance users should understand where they are and where their next vehicle is.</li>
+            <li>Design, create something elegant.</li>
+          </ul>
+          <h5>Design:</h5>
+          <p>[insert image of new tracker</p>
+          <p>Hopefully the new design speaks for itself. But to summarize</p>
+          <ul>
+            <li>I’ve continued down the dark scheme path because it does perform better with displays outside.</li>
+            <li>I’ve included a you are here marker on a map to give users a sense of where they are and where their vehicle is if in the bounded area.</li>
+          </ul>
+          <a href="#">Enjoy the prototype <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
+          </Col>
+        </Row>
+        {/* Google Earth Fly Throughs */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+          <h3 id="flythroughs">Google Earth Flythroughs</h3>
+          <p>This project is still on going so I’m limited to what I can share. This project deserves its own page.</p>
+          <a href="http://modbeta.trimet.org/">Enjoy beta <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
+          </Col>
+        </Row>
+        {/* Instagram Photo */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+            <h3 id="insta">Instagram Photo</h3>
+            <p>This project is still on going so I’m limited to what I can share. This project deserves its own page.</p>
+            <a href="http://modbeta.trimet.org/">Enjoy beta <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
+          </Col>
+        </Row>
+        {/* PDX Arrival Screens */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+            <h3 id="pdxArrival">PDX Arrival Screens</h3>
+            <p>This project is still on going so I’m limited to what I can share. This project deserves its own page.</p>
+            <a href="http://modbeta.trimet.org/">Enjoy beta <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
+          </Col>
+        </Row>
+        {/* New Club, New Blog */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+            <h3 id="ridersClub">New Club, New Blog</h3>
+            <p>This project is still on going so I’m limited to what I can share. This project deserves its own page.</p>
+            <a href="http://modbeta.trimet.org/">Enjoy beta <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
+          </Col>
+        </Row>
+        {/* Web Style Guide Lines */}
+        <Row className='my-5'>
+          <Col sm={{span:12}} md={{span:8, offset:2}}>
+            <h3 id="stylesGuide">Web Style Guide Lines</h3>
+            <p>This project is still on going so I’m limited to what I can share. This project deserves its own page.</p>
+            <a href="http://modbeta.trimet.org/">Enjoy beta <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon></a>
           </Col>
         </Row>
     </Container>
