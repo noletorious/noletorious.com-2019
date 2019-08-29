297f1343ad504770989e099808c41f2a8d8195a7
noletorious <naatorres@gmail.com>
2019-08-29 00:16:43 -0700

    Updates

--- styles-071943907c5a21455628.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{198:function(o,t,e){},204:function(o,t,e){},217:function(o,t,e){o.exports={backBtn:"navigation-module--backBtn--cyByb",n:"navigation-module--n--6sORr"}},218:function(o,t,e){o.exports={footerItem:"footer-module--footerItem--1Cju4"}},219:function(o,t,e){},227:function(o,t,e){o.exports={perProjectText:"projects-module--perProjectText--5LHly",projectTitleLogo:"projects-module--projectTitleLogo--4blgY"}}}]);
-//# sourceMappingURL=styles-071943907c5a21455628.js.map
---
+++ styles-8761a8953af3807539c4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{198:function(o,t,e){},204:function(o,t,e){},217:function(o,t,e){o.exports={backBtn:"navigation-module--backBtn--2uzti",n:"navigation-module--n--3KumS"}},218:function(o,t,e){o.exports={footerItem:"footer-module--footerItem--2ifAc"}},219:function(o,t,e){},227:function(o,t,e){o.exports={perProjectText:"projects-module--perProjectText--2JKZ1",projectTitleLogo:"projects-module--projectTitleLogo--c6zCv"}}}]);
+//# sourceMappingURL=styles-8761a8953af3807539c4.js.map
